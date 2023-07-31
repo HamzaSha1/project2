29685ff1cc89d00ead37fb599a691deb75cee671
Abbas Naser <abbas9naser@outlook.it>
2023-07-31 12:06:29 +0300

    Added Google Login / Logout

--- models/user.js
+++ models/user.js
@@ -1,5 +1,31 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const userSchema = Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  googleId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  role: {
+    type: String,
+    enum: ['Admin', 'Nurse', 'Customer'],
+    default: 'Customer',
+  },
+  address: String,
+  phoneNumber: Number,
+  avatar: String
+}, {
+  timestamps: true
+});
 
 module.exports = mongoose.model('User', userSchema);
