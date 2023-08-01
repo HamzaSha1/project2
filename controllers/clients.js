6cfb5319bf17756d730ded4cafb8369ad55c7e5c
Abbas Naser <abbas9naser@outlook.it>
2023-08-02 00:14:49 +0300

    Major Design Update & Formatting

--- controllers/clients.js
+++ controllers/clients.js
@@ -8,25 +8,33 @@ module.exports = {
 };
 
 async function index(req, res) {
-  const nurses = await User.find({
-    role: "Nurse",
-  });
+  const nurses = await User.find({ role: "Nurse" });
 
   const filteredNurses = nurses.filter((nurse) => {
     return nurse.role !== "Admin" && nurse.role !== "Customer";
   });
-  res.render("clients/clientHomePage", { title: "Home", filteredNurses });
+  
+  res.render("clients/clientHomePage", {
+    title: "List of Nurses",
+    filteredNurses
+  });
 }
 
 async function nurseDetails(req, res) {
   // Populate the cast array with performer docs instead of ObjectIds
   const nurse = await User.findById(req.params.id);
-  res.render(`clients/clientNurseDetails`, { nurse, title: "Details" });
+  res.render(`clients/clientNurseDetails`, {
+    title: "Nurse Details",
+    nurse
+  });
 }
 
 async function bookingPage(req, res) {
   const nurse = await User.findById(req.params.id);
-  res.render("clients/clientBookingPage", { title: "Nurse Booking Page", nurse });
+  res.render("clients/clientBookingPage", {
+    title: "Nurse Booking Page",
+    nurse
+  });
 }
 
 async function createReview(req, res) {
