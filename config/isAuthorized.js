76a13ca3aea16961b0c2ddab2d3b0d6dc9daa9d0
Abbas Naser <abbas9naser@outlook.it>
2023-08-06 11:38:14 +0300

    removed auth

--- config/isAuthorized.js
+++ config/isAuthorized.js
@@ -3,7 +3,7 @@ module.exports = function(req, res, next) {
 
   // Pass the req/res to the next middleware/route handler if the user is authenticated
   // Check if the user ID in the request matches the logged-in user's ID
-  if (req.isAuthenticated() && req.params.id && req.user && req.params.id === req.user._id.toString()) {
+  if (req.isAuthenticated()) {
     return next();
   } else {
     // User is trying to access resources of other users, redirect to their own profile page or show an error
