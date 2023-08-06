8471207ce52ae9aef5de30c5710afb8a3bacfbbc
Abbas Naser <abbas9naser@outlook.it>
2023-08-06 19:20:51 +0300

    [Final Project]

--- config/isAuthorized.js
+++ config/isAuthorized.js
@@ -3,7 +3,7 @@ module.exports = function(req, res, next) {
 
     // Pass the req/res to the next middleware/route handler if the user is authenticated
     // Check if the user ID in the request matches the logged-in user's ID
-    if (req.isAuthenticated()) {
+    if (req.isAuthenticated() && user.role === 'Customer' || user.role === 'Nurse') {
       return next();
     } else {
       // User is trying to access resources of other users, redirect to their own profile page or show an error
